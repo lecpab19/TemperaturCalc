7987bf0b1c2990014579bb8cc9a75a4a686a0a18
Paul <lecpab19@new.edvhtl.at>
2021-10-04 11:13:32 +0200

    Adding constants and methods

--- app/src/main/java/at/htlkaindorf/temperaturecalc/MainActivity.java
+++ app/src/main/java/at/htlkaindorf/temperaturecalc/MainActivity.java
@@ -3,12 +3,24 @@ package at.htlkaindorf.temperaturecalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button calc;
+    private final double CelsiusToKalvinCourse = 273.15;
+    private final double CelsiusToFahrenheitCourse = 32;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void CToK(double input){
+        double result = input + CelsiusToKalvinCourse;
+    }
+
+    private void CToF(double input){
+        double result = input * (9/5) + CelsiusToFahrenheitCourse;
     }
 }
