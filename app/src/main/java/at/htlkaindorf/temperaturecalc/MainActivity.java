aadd7299e783d00ab8cfc5350c8282c7629158d7
Paul <lecpab19@new.edvhtl.at>
2021-10-11 10:56:48 +0200

    Changing update method

--- app/src/main/java/at/htlkaindorf/temperaturecalc/MainActivity.java
+++ app/src/main/java/at/htlkaindorf/temperaturecalc/MainActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button calc;
     private Editable CInput;
-    private Editable FOutput;
-    private Editable KOutput;
+    private TextView FOutput;
+    private TextView KOutput;
     private double resultK;
     private double resultF;
     private final double CelsiusToKalvinCourse = 273.15;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //CInput = (Editable) findViewById(R.id.tvInValue);
     }
 
     private void CToK(double input){
@@ -31,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void update(){
-        FOutput.setText(resultF);
-        KOutput.setText(resultK);
+        FOutput.setText(String.format("%.2f", resultF));
+        KOutput.setText(String.format("%.2f", resultK));
     }
 }
