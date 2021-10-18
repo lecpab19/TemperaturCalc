4f7469c7b6021305d34d504b16d9b90b079af7e9
Paul <lecpab19@new.edvhtl.at>
2021-10-18 11:37:30 +0200

    Adding methods for calculation

--- app/src/main/java/at/htlkaindorf/temperaturecalc/MainActivity.java
+++ app/src/main/java/at/htlkaindorf/temperaturecalc/MainActivity.java
@@ -12,10 +12,11 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private Button calc;
     private EditText CInput;
-    private TextView FOutput;
-    private TextView KOutput;
+    private EditText FOutput;
+    private EditText KOutput;
     private double resultK;
     private double resultF;
+    private double resultC;
     private final double CelsiusToKalvinCourse = 273.15;
     private final double CelsiusToFahrenheitCourse = 32;
 
@@ -24,19 +25,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CInput = (EditText) findViewById(R.id.edInput);
+        CInput = (EditText) findViewById(R.id.tvOutValTempC);
         calc = (Button) findViewById(R.id.btCalc);
-        FOutput = (TextView) findViewById(R.id.tvOutValTempF);
-        KOutput = (TextView) findViewById(R.id.tvOutValTempK);
+        FOutput = (EditText) findViewById(R.id.tvOutValTempF);
+        KOutput = (EditText) findViewById(R.id.tvOutValTempK);
 
         calc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
-                    double input = getInput();
+                    int index = readScreen();
+                    /*double input = getInput();
                     CToF(input);
                     CToK(input);
-                    update();
+                    update();*/
                 } catch (NumberFormatException ex){
                     ex.printStackTrace();
                 }
@@ -44,24 +46,71 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void manage(int index){
+        double value;
+        switch (index){
+            case 1:
+                value = getInput(CInput);
+                CToF(value);
+                CToF(value);
+                break;
+            case 2:
+                value = getInput(KOutput);
+                KToC(value);
+                KToF(value);
+                break;
+            case 3:
+                value = getInput(FOutput);
+                FToC(value);
+                FToK(value);
+                break;
+        }
+    }
+
+    private int readScreen(){
+        int index = 0;
+        if(!CInput.getText().toString().equals("")){
+            index = 1;
+        } else if(!FOutput.getText().toString().equals("")){
+            index = 2;
+        } else if(!KOutput.getText().toString().equals("")){
+            index = 3;
+        }
+
+        return index;
+    }
+
     private void CToK(double input){
         resultK = input + CelsiusToKalvinCourse;
+        KOutput.setText(String.format("%.2f", resultK));
     }
 
     private void CToF(double input){
         resultF = input * 1.8 + CelsiusToFahrenheitCourse;
+        FOutput.setText(String.format("%.2f", resultF));
     }
 
-    private double getInput() throws NumberFormatException{
+    private void FToC(double input){
+        resultC = (input - CelsiusToFahrenheitCourse) / 1.8;
+    }
+
+    private void FToK(double input){
+
+    }
+
+    private void KToC(double input){
+        resultC = input - CelsiusToKalvinCourse;
+    }
+
+    private void KToF(double input){
+
+    }
+
+    private double getInput(EditText text) throws NumberFormatException{
         double res = 0.0;
-        String help = CInput.getText().toString();
+        String help = text.getText().toString();
         res = Double.parseDouble(help);
 
         return res;
-    }
-
-    private void update(){
-        FOutput.setText(String.format("%.2f", resultF));
-        KOutput.setText(String.format("%.2f", resultK));
     }
 }
